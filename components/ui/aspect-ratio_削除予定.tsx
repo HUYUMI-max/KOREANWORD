2eeedb3e90b8d2942dcf5cb8fad444c770914e1f
HUYUMI <mau0gefu101.lovejanne@outlook.jp>
2025-03-23 20:05:01 +0900

    使わないuiファイルをrenameし、コメントアウトしました

---
+++ components/ui/aspect-ratio_削除予定.tsx
@@ -0,0 +1,8 @@
+// このファイルは現在未使用です（削除予定）
+// 'use client';
+// 
+// import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
+// 
+// const AspectRatio = AspectRatioPrimitive.Root;
+// 
+// export { AspectRatio };
---
+++ components/ui/collapsible_削除予定.tsx
@@ -0,0 +1,12 @@
+// ?? ª³ªÎ«Õ«¡«¤«ëªÏúÞî¤Ú±ÞÅéÄªÇª¹£¨Þûð¶åøïÒ£©
+// 'use client';
+// 
+// import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
+// 
+// const Collapsible = CollapsiblePrimitive.Root;
+// 
+// const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+// 
+// const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+// 
+// export { Collapsible, CollapsibleTrigger, CollapsibleContent };
